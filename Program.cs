34838d360050fba5b30d8e41869584b7d8cb251a
Ellianesh <mark.voznyi@gmail.com>
2023-03-17 15:15:58 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -16,4 +16,19 @@ void SecondArrayWithIF(string[] array1, string[] array2)
         count++;
         }
     }
-    // конец второго коммита //
+    
+    void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+   
+    // тут должен закончится 3 коммит (но это не точно)
+
+
+
+    // думали это конец коммита? Нет, это я, Дио!
+
+    //А вот тут уже точно конец
