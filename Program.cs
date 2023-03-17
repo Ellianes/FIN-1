4e418307cafb60d67b2568f2d9172760a220cbf2
Ellianesh <mark.voznyi@gmail.com>
2023-03-17 15:08:46 +0300

    #1
    коммит первый

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿/* Написать программу, которая из имеющегося массива строк 
+формирует массив из строк, длина которых меньше или равно 3
+*/
+
+string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
+string[] array2 = new string[array1.Length];
+void SecondArrayWithIF(string[] array1, string[] array2)
+
+//конец первого комита //
